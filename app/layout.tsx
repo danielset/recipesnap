2b20112b8a265ccabe7e5be47421d5bfcd12da4e
Daniel Setzermann <hello@danielsetzermann.com>
2024-11-06 21:42:31 +0100

    removed webmanifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#FFFFFF" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </head>
